7e296ac251ac5bc956dbd3fc348f738a52c7f0e1
Michael Matranga <mike@dmgctrl.com>
2014-12-03 20:21:53 -0500

    Hitting an url and parsing some JSON.

--- EarthQuake/ViewController.swift
+++ EarthQuake/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  EarthQuake
-//
-//  Created by Michael Matranga on 9/27/14.
-//  Copyright (c) 2014 Michael Matranga. All rights reserved.
-//
-
 import UIKit
 import Alamofire
 
@@ -13,17 +5,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        Alamofire.request(.GET, "http://http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_hour.geojson")
-                 .responseJSON {(request, response, JSON, error) in
-                     println(JSON)
-                 }
+        Alamofire
+            .request(.GET, "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson")
+            .responseJSON {(request, response, JSON, error) in
+                println(request)
+                println(response)
+                println(JSON)
+            }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
