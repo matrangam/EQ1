6a247058b04b4e595c252319aa99bbcf1d3fef59
Michael Matranga <mike@dmgctrl.com>
2015-01-07 16:40:18 -0500

    Added a button to get EQ data.

--- EarthQuake/ViewController.swift
+++ EarthQuake/ViewController.swift
@@ -3,14 +3,23 @@ import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet var _earthquakeButton: UIButton!
+    
+    @IBAction func _earthquakeButtonPressed(sender: AnyObject) {
+        _getEarthquakeData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func _getEarthquakeData() {
         Alamofire
             .request(.GET, "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson")
             .responseJSON {(request, response, JSON, error) in
                 println(request)
                 println(response)
                 println(JSON)
-            }
+        }
     }
 }
